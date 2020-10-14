96a12b275612bfa9db3342dd824d0d9252c55d93
Diego <diego.luz.braga@gmail.com>
2020-10-13 22:08:25 -0300

    chore: fix errors and add baseRequest

--- src/stores/AuthStore/__abstract__/index.ts
+++ src/stores/AuthStore/__abstract__/index.ts
@@ -1,28 +1,3 @@
-import { action, observable } from "mobx";
-import { UserData } from "../../../@types/interfaces";
-
 export abstract class AbstractAuthStore {
 	abstract handleLogin: () => Promise<void>;
-
-	@observable
-	public userData: UserData = {
-		userName: "",
-		password: "",
-	};
-
-	@observable
-	public hasError: boolean = false;
-
-	@action
-	public setUserData = (field: keyof UserData, value: string) => {
-		this.userData = {
-			...this.userData,
-			[field]: value,
-		};
-	};
-
-	@action
-	public setHasError = (value: boolean) => {
-		this.hasError = value;
-	};
 }
--- src/stores/RootStore/__mocks__/index.ts
+++ src/stores/RootStore/__mocks__/index.ts
@@ -3,13 +3,11 @@ import { History, createBrowserHistory } from "history";
 import { createContext } from "react";
 import { AbstractRootStore } from "../__abstract__";
 import { AuthStore } from "../../AuthStore";
-import { NewsStore } from "../../NewsStore";
 
 export class RootStoreMock extends AbstractRootStore {
 	public routerStore: RouterStore;
 	public history: History;
 	public authStore: AuthStore;
-	public newsStore: NewsStore;
 
 	public constructor() {
 		super();
@@ -17,13 +15,11 @@ export class RootStoreMock extends AbstractRootStore {
 		this.routerStore = new RouterStore();
 		this.history = syncHistoryWithStore(browerHistory, this.routerStore);
 		this.authStore = new AuthStore();
-		this.newsStore = new NewsStore();
 
 		return {
 			routerStore: this.routerStore,
 			history: this.history,
 			authStore: this.authStore,
-			newsStore: this.newsStore,
 		};
 	}
 }
